309bbaa0e4b69713dc027b7056e3db82668dc4dd
Anastasiia Diachenko <anastasiiadiachenko1@gmail.com>
2021-08-04 13:26:06 +0300

    Updates

---
+++ precache-manifest.8b2a54d5ab02445e553bce1ef53c9dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f66a4ef29eb6b1f77b66972b333f4f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d59f54814ae5fdb5373",
+    "url": "./static/css/main.07c9579c.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d59f54814ae5fdb5373",
+    "url": "./static/js/main.c19f6fc5.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
